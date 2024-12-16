6421f0f26a3aa26e891084e577e0a58fa8b33ce0
shabbir <shabbirhossain660@gmail.com>
2024-12-16 15:46:30 +0600

    first commit

---
+++ components/Que.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Que = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Que;
---
+++ tailwind.config.js
@@ -0,0 +1,89 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: ["class"],
+	content: [
+	  './pages/**/*.{js,jsx}',
+	  './components/**/*.{js,jsx}',
+	  './app/**/*.{js,jsx}',
+	  './src/**/*.{js,jsx}',
+	],
+	prefix: "",
+	theme: {
+    	container: {
+    		center: 'true',
+    		padding: '2rem',
+    		screens: {
+    			sm: '100%',
+    			md: '768px',
+    			lg: '960px',
+    			xl: '1200px',
+    			'2xl': '1440px',
+    			hd: '1920px'
+    		}
+    	},
+    	extend: {
+    		colors: {
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			primary: '#1c1c22',
+    			accent: {
+    				DEFAULT: '#00ff99',
+    				hover: '#00e187'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			},
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
+    		}
+    	},
+    	fontFamily: {
+    		primary: 'var(--font-vietnam)'
+    	}
+    },
+	plugins: [require("tailwindcss-animate")],
+  }
